b1bc295e89a2961fe6e7123530aaa5f9ee50ca1c
Magician960 <tint.nuyet1962@gmail.com>
2023-01-14 21:25:22 +1100

    Changed folder names, added comments to grade_sorter_app

--- Chapter_2/grade_sorter_app.py
+++ Chapter_2/grade_sorter_app.py
@@ -3,24 +3,32 @@
 #dropping the lowest two grades the user entered. Lastly, it will comment on the users highest
 #grade.
 
+#Initialise list to hold grades
 grades = []
 
+#Introductory message
 print("Welcome to the Grade Sorter App")
 
+#Asks user for 4 grades
 grades.append(int(input("\nWhat is your first grade (0-100): ")))
 grades.append(int(input("What is your second grade (0-100): ")))
 grades.append(int(input("What is your third grade (0-100): ")))
 grades.append(int(input("What is your fourth grade (0-100): ")))
 
+#Prints grades
 print(f"\nYour grades are: {grades}")
 
+#Sorts grades in descending order
 grades.sort(reverse = True)
 
+#Shows sorted grades list
 print(f"\nYour grades from highest to lowest are: {grades}")
 
+#Drops and shows lowest two grades
 print("\nThe lowest two grades will now be dropped.")
 
 print(f"Removed grade: {grades.pop()}")
 print(f"Removed grade: {grades.pop()}")
 
+#Show remaining grades
 print(f"\nYour remaining grades are: {grades}\nNice Work! Your highest grade is a {grades[0]}.")
